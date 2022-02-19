2d0643c01b4374a3d0a7b9142329e498945bb822
Salim Oussayfi <box@slimou.net>
2022-02-19 11:59:14 +0100

    renaming to context as it be a dao

--- JPADemo/src/main/java/net/slimou/jpa/demo/FarmerDao.java
+++ JPADemo/src/main/java/net/slimou/jpa/demo/FarmerDao.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class JpaDemo {
+public class FarmerDao {
 
 	private static final EntityManagerFactory emFactoryObj;
 	private static final String PERSISTENCE_UNIT_NAME = "JPADemo";
