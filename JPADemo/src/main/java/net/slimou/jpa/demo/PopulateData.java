86e240c383f67fa70fd9dfc9ada33bc80c9b9160
Salim Oussayfi <box@slimou.net>
2022-02-19 12:16:40 +0100

    renaming populate class

--- JPADemo/src/main/java/net/slimou/jpa/demo/PopulateData.java
+++ JPADemo/src/main/java/net/slimou/jpa/demo/PopulateData.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class FarmerDao {
+public class PopulateData {
 
 	private static final EntityManagerFactory emFactoryObj;
 	private static final String PERSISTENCE_UNIT_NAME = "JPADemo";
